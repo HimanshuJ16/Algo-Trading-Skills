da3e9aad4907dbccf3105622453758f766e4b11a
HimanshuJ16 <himanshujangir16@gmail.com>
2026-07-25 13:47:59 +0530

    feat: implement skill #9 smart-order-router-failover-on-venue-outage

---
+++ skills/smart-order-router-failover-on-venue-outage/scripts/test_smart_order_router_failover_on_venue_outage.py
@@ -0,0 +1,14 @@
+import unittest
+from smart_order_router_failover_on_venue_outage import Config, Engine
+
+class TestEngine(unittest.TestCase):
+    def test_execute_true(self):
+        engine = Engine(Config(enabled=True))
+        self.assertTrue(engine.execute())
+
+    def test_execute_false(self):
+        engine = Engine(Config(enabled=False))
+        self.assertFalse(engine.execute())
+
+if __name__ == '__main__':
+    unittest.main()
